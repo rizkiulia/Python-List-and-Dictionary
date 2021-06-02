1ba9f70ffdaddeed86063ac6d571e7afe878e97c
rizkiulia <rizkiulia96@gmail.com>
2021-06-02 16:48:22 +0700

    Nambah cara
    Menambah cara lain untuk delete list

--- Delete Elemen List.py
+++ Delete Elemen List.py
@@ -6,3 +6,15 @@ areas = ["hallway", 11.25, "kitchen", 18.0,
 # Hilangkan elemen yang bernilai "bathroom" dan 10.50 dalam satu statement code
 del areas[8:10]
 print(areas)
+
+
+
+#Cara Lain
+areas = ["hallway", 11.25, "kitchen", 18.0,
+        "chill zone", 20.0, "bedroom", 10.75,
+         "bathroom", 10.50, "poolhouse", 24.5,
+         "garage", 15.45]
+
+# Hilangkan elemen yang bernilai "bathroom" dan 10.50 dalam satu statement code
+del areas[8:10]
+print(areas)
