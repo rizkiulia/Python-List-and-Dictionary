3b667690f2daf3b2aa61122b8165070bb3869e03
rizkiulia <rizkiulia96@gmail.com>
2021-06-02 16:51:06 +0700

    Create Akses Dictionary.py

---
+++ Akses Dictionary.py
@@ -0,0 +1,6 @@
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# print semua key yang ada di objek europe
+print(europe.keys())
+# print nilai dari key franche
+print(europe['france'])
