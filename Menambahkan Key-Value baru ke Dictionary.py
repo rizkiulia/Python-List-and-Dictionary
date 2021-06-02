21e372f1196455a16c80e3fa5811c9516bf9fc19
rizkiulia <rizkiulia96@gmail.com>
2021-06-02 16:51:33 +0700

    Create Menambahkan Key-Value baru ke Dictionary.py

---
+++ Menambahkan Key-Value baru ke Dictionary.py
@@ -0,0 +1,7 @@
+europe = {'spain':'madrid', 'france':'paris', 'germany':'berlin', 'norway':'oslo' }
+
+# tambahkan key itali ke objek dictionary dengan value roma
+europe['itali'] = 'roma'
+
+# cek apakah itali ada di dalam objek dictionary
+print('itali' in europe)
