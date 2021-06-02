900b2135f812bd48a0de07e3eb083d7f4efc0a2a
rizkiulia <rizkiulia96@gmail.com>
2021-06-02 15:10:40 +0700

    Create Delete Elemen List.py

---
+++ Delete Elemen List.py
@@ -0,0 +1,8 @@
+areas = ["hallway", 11.25, "kitchen", 18.0,
+        "chill zone", 20.0, "bedroom", 10.75,
+         "bathroom", 10.50, "poolhouse", 24.5,
+         "garage", 15.45]
+
+# Hilangkan elemen yang bernilai "bathroom" dan 10.50 dalam satu statement code
+del areas[8:10]
+print(areas)
